90a6f94052aec5839dd7941e3ae37bfc9fbe6b08
Remco Tolsma <info@remcotolsma.nl>
2014-06-06 16:02:46 +0200

    WordPress Coding Standards optimizations.

--- stock-synchronization.php
+++ stock-synchronization.php
@@ -1,59 +1,58 @@
 <?php
 /*
 Plugin Name: WooCommerce Stock Synchronization
-Plugin URI: http://pronamic.eu
+Plugin URI: http://www.happywp.com/plugins/woocommerce-stock-synchronization/
 Description: Synchronizes stock with sites that are connected to one another, using WooCommerce Stock Synchronization.
 
 Version: 1.1.2
 Requires at least: 3.0
 
 Author: Pronamic
-Author URI: http://pronamic.eu/
+Author URI: http://www.pronamic.eu/
 
 Text Domain: stock-synchronization
 Domain Path: /languages/
 
 License: GPLv2
 
-GitHub URI: 
+GitHub URI: https://github.com/pronamic/wp-woocommerce-stock-synchronization
 */
 
 /**
  * Main class, bootstraps the plugin
  */
 class Stock_Synchronization {
-	
 	/**
 	 * The plugin file
-	 * 
+	 *
 	 * @var string
 	 */
 	public static $file;
-	
+
 	/**
 	 * Log option name
-	 * 
+	 *
 	 * @var string
 	 */
 	public static $log_option_name = 'stock-synchronization-log';
-	
+
 	/**
 	 * Maximum log length in number of rows
-	 * 
+	 *
 	 * @var int
 	 */
 	public static $max_log_length = 50;
-	
+
 	/**
 	 * Sites to notify of stock change
-	 * 
+	 *
 	 * @var mixed
 	 */
 	public static $synced_sites;
-	
+
 	/**
 	 * Password shared among sites in sychronization network
-	 * 
+	 *
 	 * @var string
 	 */
 	public static $synced_sites_password;
@@ -62,19 +61,19 @@ class Stock_Synchronization {
 
 	/**
 	 * Bootstrap
-	 * 
+	 *
 	 * @param string file path and name
 	 */
 	public static function bootstrap( $file ) {
 		self::$file = $file;
 
 		self::autoload();
-		
+
 		self::$synced_sites          = get_option( 'woocommerce_stock_sync_urls', array() );
 		self::$synced_sites_password = get_option( 'woocommerce_stock_sync_password' );
-		
+
 		Stock_Synchronization_Synchronizer::Bootstrap();
-		
+
 		if ( is_admin() ) {
 			Stock_Synchronization_Admin::Bootstrap();
 		}
@@ -108,8 +107,9 @@ class Stock_Synchronization {
 	 * This function will load classes automatically on-call.
 	 */
 	public static function autoload() {
-		if( ! function_exists( 'spl_autoload_register' ) )
+		if ( ! function_exists( 'spl_autoload_register' ) ) {
 			return;
+		}
 
 		function stock_synchronization_autoload( $name ) {
 			$name = strtolower( str_replace( '_', '-', $name ) );
