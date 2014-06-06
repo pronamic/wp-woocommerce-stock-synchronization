0df71fe6bdfc216229d44551fadd0b5e84a18f40
Remco Tolsma <info@remcotolsma.nl>
2014-06-06 16:00:58 +0200

    Added Grunt to automate some tasks.

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+module.exports = function( grunt ) {
+	// Project configuration.
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		// PHPLint
+		phplint: {
+			options: {
+				phpArgs: {
+					'-lf': null
+				}
+			},
+			all: [ '**/*.php' ]
+		},
+
+		// PHP Code Sniffer
+		phpcs: {
+			application: {
+				dir: [ './' ],
+			},
+			options: {
+				standard: 'project.ruleset.xml',
+				extensions: 'php',
+				ignore: 'wp-svn,deploy,node_modules'
+			}
+		},
+
+		// Check WordPress version
+		checkwpversion: {
+			options: {
+				readme: 'readme.txt',
+				plugin: 'stock-synchronization.php',
+			},
+			check: {
+				version1: 'plugin',
+				version2: 'readme',
+				compare: '=='
+			},
+			check2: {
+				version1: 'plugin',
+				version2: '<%= pkg.version %>',
+				compare: '=='
+			}
+		},
+
+		// Make POT
+		makepot: {
+			target: {
+				options: {
+					cwd: '',
+					domainPath: 'languages',
+					type: 'wp-plugin',
+					exclude: [ 'deploy/.*', 'wp-svn/.*' ],
+				}
+			}
+		},
+	} );
+
+	grunt.loadNpmTasks( 'grunt-phplint' );
+	grunt.loadNpmTasks( 'grunt-phpcs' );
+	grunt.loadNpmTasks( 'grunt-checkwpversion' );
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+
+	// Default task(s).
+	grunt.registerTask( 'default', [ 'phplint', 'phpcs', 'checkwpversion' ] );
+};
